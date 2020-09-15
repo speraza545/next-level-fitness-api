b72ae333446f3979f791ee5773bebba1ade6be10
skmidk <s.peraza545@gmail.com>
2020-09-15 18:36:48 -0400

    quick fix

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'next-level-fitness-client'
+const mongooseBaseName = 'next-level-fitness-api'
 
 // create the mongodb uri for development and test
 const database = {
